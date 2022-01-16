71ce1ad0015390d2342d5ff7e39e5871a9a7dafe
MrTrukiny <carloseduardo.tkn@gmail.com>
2022-01-16 08:14:08 -0500

    Introduce 'array' TypeScript type

--- app.ts
+++ app.ts
@@ -2,9 +2,19 @@
 //   name: string;
 //   age: number;
 // } = {
-const person = {
-  name: 'Eduardo',
-  age: 30,
-};
-
-console.log(person.name);
+  const person = {
+    name: 'Eduardo',
+    age: 30,
+    hobbies: ['Sports', 'Cooking']
+  };
+  
+  let favoriteActivities: string[];
+  favoriteActivities = ['Sports'];
+  
+  console.log(person.name);
+  
+  for (const hobby of person.hobbies) {
+    console.log(hobby.toUpperCase());
+    // console.log(hobby.map()); // !!! ERROR !!!
+  }
+  
