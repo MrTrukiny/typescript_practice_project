c7424508740f875a3c6f7532ce3defdcb8f06329
MrTrukiny <carloseduardo.tkn@gmail.com>
2022-01-16 08:20:18 -0500

    Introduce 'tuple' TypeScript type

--- app.ts
+++ app.ts
@@ -1,20 +1,26 @@
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-  const person = {
-    name: 'Eduardo',
-    age: 30,
-    hobbies: ['Sports', 'Cooking']
-  };
-  
-  let favoriteActivities: string[];
-  favoriteActivities = ['Sports'];
-  
-  console.log(person.name);
-  
-  for (const hobby of person.hobbies) {
-    console.log(hobby.toUpperCase());
-    // console.log(hobby.map()); // !!! ERROR !!!
-  }
-  
+const person: {
+  name: string;
+  age: number;
+  hobbies: string[];
+  role: [number, string];
+} = {
+  name: 'Eduardo',
+  age: 30,
+  hobbies: ['Sports', 'Cooking'],
+  role: [2, 'author'],
+};
+
+// person.role.push('admin');
+// person.role[1] = 10;
+
+// person.role = [0, 'admin', 'user'];
+
+let favoriteActivities: string[];
+favoriteActivities = ['Sports'];
+
+console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+  // console.log(hobby.map()); // !!! ERROR !!!
+}
